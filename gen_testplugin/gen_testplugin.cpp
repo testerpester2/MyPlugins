5ae58cb8143843a612c933dffa942c2763e9fb82
testerpester2 <gumgettergaminglol@gmail.com>
2026-04-13 16:49:43 -0400

    mmm yummy.

--- gen_testplugin/gen_testplugin.cpp
+++ gen_testplugin/gen_testplugin.cpp
@@ -16,7 +16,6 @@ Thanks for the help everyone!
 // these are callback functions/events which will be called by Winamp
 int    init(void);
 void    config(void);
-void    quit(void);
  
 
 // this structure contains plugin information, version, name...
@@ -26,7 +25,6 @@ winampGeneralPurposePlugin plugin = {
     PLUGIN_NAME,    // name/title of the plugin, defined in "gen_myplugin.h"
     init,           // function name which will be executed on init event
     config,         // function name which will be executed on config event
-    quit,           // function name which will be executed on quit event
     0,              // handle to Winamp main window, loaded by winamp when this dll is loaded
     0               // hinstance to this dll, loaded by winamp when this dll is loaded
 };
@@ -47,14 +45,6 @@ void config() {
     //You can change this later to do whatever you want (including nothing)
     MessageBox(plugin.hwndParent, "Config event triggered for gen_testplugin.", "", MB_OK);
 }
- 
-void quit() {
-    //A basic messagebox that tells you the 'quit' event has been triggered.
-    //If everything works you should see this message when you quit Winamp once your plugin has been installed.
-    //You can change this later to do whatever you want (including nothing)
-    MessageBox(0, "Quit event triggered for gen_testplugin.", "", MB_OK);
-}
- 
  
 // This is an export function called by winamp which returns this plugin info.
 // We wrap the code in 'extern "C"' to ensure the export isn't mangled if used in a CPP file.
